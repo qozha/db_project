cdf1fc44305c6910ae6ddbce64ef3e2d4a4477a6
qozha <madiabsadykov@gmail.com>
2021-12-07 16:14:27 +0600

    fixes

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,4 +4,4 @@
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
-gunicorn djangoProject.wsgi:application --bind 0.0.0.0:4000
+gunicorn hw2.wsgi:application --bind 0.0.0.0:4000
